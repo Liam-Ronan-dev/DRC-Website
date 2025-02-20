b139a7cb6ce70fe91ff023c0f60844a02cf5938b
Liam Ronan <liamronan16@gmail.com>
2025-02-20 20:20:24 +0000

    created astro, tailwind, daisyui template

---
+++ prettier.config.js
@@ -0,0 +1,18 @@
+/** @type {import("prettier").Config} */
+export default {
+  singleQuote: true,
+  trailingComma: "es5",
+  printWidth: 80,
+  tabWidth: 2,
+  semi: true,
+  endOfLine: "auto",
+  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
