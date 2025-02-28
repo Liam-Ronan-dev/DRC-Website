142da602a63bd65fce4415962028b7ed300f1da9
Liam-Ronan-dev <liamronan16@gmail.com>
2025-02-28 23:47:37 +0000

    Built components, nav, link, button, fonts, toDo: Layout file

--- prettier.config.js
+++ prettier.config.js
@@ -1,17 +1,17 @@
 /** @type {import("prettier").Config} */
 export default {
   singleQuote: true,
-  trailingComma: "es5",
+  trailingComma: 'es5',
   printWidth: 80,
   tabWidth: 2,
   semi: true,
-  endOfLine: "auto",
-  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+  endOfLine: 'auto',
+  plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
   overrides: [
     {
-      files: "*.astro",
+      files: '*.astro',
       options: {
-        parser: "astro",
+        parser: 'astro',
       },
     },
   ],
